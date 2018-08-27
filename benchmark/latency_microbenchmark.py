4ce8952374328f1d6cf550ecafa5aed4a6c3dda2
Stephanie <swang@cs.berkeley.edu>
2018-08-27 16:27:15 -0700

    Modify latency microbenchmark to print time since submission

--- benchmark/latency_microbenchmark.py
+++ benchmark/latency_microbenchmark.py
@@ -10,7 +10,6 @@ import argparse
 import numpy as np
 
 BATCH_SIZE = 1000
-NUM_ITEMS = 10 * 1000
 
 class A(object):
     def __init__(self, node_resource):
@@ -22,24 +21,32 @@ class A(object):
         time.sleep(1)
         return True
 
-    def f(self):
+    def f(self, target_throughput, experiment_time):
         time_str = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f')
-        print("push, start " + time_str)
-        latencies = []
-        for i in range(0, NUM_ITEMS, BATCH_SIZE):
-            start = time.time()
-            for _ in range(0, BATCH_SIZE):
-                ray.get(self.b.f.remote())
-            end = time.time()
-            latency = end - start
-            print("push, latency round ", i , ":", latency / BATCH_SIZE)
-            latencies.append(latency)
-        return latencies
+        start = time.time()
+        start2 = time.time()
+        i = 0
+        batch_size = BATCH_SIZE
+        if target_throughput < batch_size:
+            batch_size = int(target_throughput)
+        while True:
+            self.b.f.remote(time.time())
+            if i % batch_size == 0 and i > 0:
+                end = time.time()
+                sleep_time = (batch_size / target_throughput) - (end - start2)
+                if sleep_time > 0.00003:
+                    time.sleep(sleep_time)
+                start2 = time.time()
+                if end - start >= experiment_time:
+                    break
+            i += 1
+
+        return ray.get(self.b.get_sum.remote())
 
 class B(object):
     def __init__(self):
         print("Actor B start...")
-        self.sum = 0
+        self.latencies = []
         #import yep
         #yep.start('actorB.prof')
 
@@ -47,20 +54,23 @@ class B(object):
         time.sleep(1)
         return True
 
-    def f(self):
-        self.sum += 1
-        return 1
+    def f(self, submit_time):
+        latency = time.time() - submit_time
+        self.latencies.append(latency)
+        if len(self.latencies) % BATCH_SIZE == 0:
+            print(latency)
 
 
     def get_sum(self):
         #import yep
         #yep.stop()
-        return self.sum
+        return self.latencies
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--target-throughput', type=int, default=3000)
+    parser.add_argument('--experiment-time', type=int, default=10)
     parser.add_argument('--num-workers', type=int, default=1)
     parser.add_argument('--num-raylets', type=int, default=1)
     parser.add_argument('--num-shards', type=int, default=None)
@@ -96,7 +106,11 @@ if __name__ == "__main__":
                 })(A)
             actors.append(actor_cls.remote("Node{}".format(node * 2 + 1)))
     total_time = ray.get([actor.ready.remote() for actor in actors])
-    latencies = ray.get([actor.f.remote() for actor in actors])
+    latencies = ray.get([actor.f.remote(
+        args.target_throughput,
+        args.experiment_time) for actor in actors])
     latencies = [latency[len(latency) // 4 : 3 * len(latency) // 4] for latency in latencies]
     for latency in latencies:
-        print("DONE, average latency:", sum(latency) / len(latency) / BATCH_SIZE * 1000000, "us, variance:", np.var(latency))
+        print("DONE, average latency:", sum(latency) / len(latency))
+        for point in latency:
+            print(point)
