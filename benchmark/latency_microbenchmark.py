d9280ebef4db241653352deb84a6671cd206ead0
Stephanie <swang@cs.berkeley.edu>
2018-08-28 06:33:03 +0000

    Ping pong tasks in latency microbenchmark

--- benchmark/latency_microbenchmark.py
+++ benchmark/latency_microbenchmark.py
@@ -9,12 +9,16 @@ import time
 import argparse
 import numpy as np
 
-BATCH_SIZE = 1000
+BATCH_SIZE = 100
+
+@ray.remote
+def pong(submit_time):
+    return time.time() - submit_time
 
 class A(object):
-    def __init__(self, node_resource):
+    def __init__(self, ping_node_resource, pong_node_resource):
         print("Actor A start...")
-        self.b = ray.remote(resources={node_resource: 1})(B).remote()
+        self.b = ray.remote(resources={ping_node_resource: 1})(B).remote(pong_node_resource)
         ray.get(self.b.ready.remote())
 
     def ready(self):
@@ -30,7 +34,7 @@ class A(object):
         if target_throughput < batch_size:
             batch_size = int(target_throughput)
         while True:
-            self.b.f.remote(time.time())
+            self.b.ping.remote(time.time())
             if i % batch_size == 0 and i > 0:
                 end = time.time()
                 sleep_time = (batch_size / target_throughput) - (end - start2)
@@ -44,34 +48,36 @@ class A(object):
         return ray.get(self.b.get_sum.remote())
 
 class B(object):
-    def __init__(self):
+    def __init__(self, node_resource):
         print("Actor B start...")
         self.latencies = []
         #import yep
         #yep.start('actorB.prof')
+        self.node_resource = node_resource
 
     def ready(self):
         time.sleep(1)
         return True
 
-    def f(self, submit_time):
-        latency = time.time() - submit_time
-        self.latencies.append(latency)
-        if len(self.latencies) % BATCH_SIZE == 0:
-            print(latency)
-
+    def ping(self, submit_time):
+        self.latencies.append(pong._submit(
+                args=[submit_time],
+                resources={
+                    self.node_resource: 1,
+                    }))
 
     def get_sum(self):
         #import yep
         #yep.stop()
-        return self.latencies
+        return ray.get(self.latencies)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--target-throughput', type=int, default=3000)
+    parser.add_argument('--target-throughput', type=int, default=1000)
     parser.add_argument('--experiment-time', type=int, default=10)
     parser.add_argument('--num-workers', type=int, default=1)
+    parser.add_argument('--pingpong', action='store_true')
     parser.add_argument('--num-raylets', type=int, default=1)
     parser.add_argument('--num-shards', type=int, default=None)
     parser.add_argument('--gcs', action='store_true')
@@ -79,6 +85,9 @@ if __name__ == "__main__":
     parser.add_argument('--redis-address', type=str)
     parser.add_argument('--max-lineage-size', type=str)
     args = parser.parse_args()
+
+    if args.pingpong:
+        args.target_throughput //= 2
 
     gcs_delay_ms = -1
     if args.gcs:
@@ -101,10 +110,19 @@ if __name__ == "__main__":
     actors = []
     for node in range(args.num_raylets):
         for _ in range(args.num_workers):
+            send_resource = "Node{}".format(node * 2)
+            receive_resource = "Node{}".format(node * 2 + 1)
             actor_cls = ray.remote(resources={
-                "Node{}".format(node * 2): 1,
+                send_resource: 1,
                 })(A)
-            actors.append(actor_cls.remote("Node{}".format(node * 2 + 1)))
+            actors.append(actor_cls.remote(receive_resource, send_resource))
+
+            if args.pingpong:
+                actor_cls = ray.remote(resources={
+                    receive_resource: 1
+                    })(A)
+                actors.append(actor_cls.remote(send_resource, receive_resource))
+
     total_time = ray.get([actor.ready.remote() for actor in actors])
     latencies = ray.get([actor.f.remote(
         args.target_throughput,
