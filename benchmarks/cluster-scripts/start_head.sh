c9cabf69674dd31668c54acb353b7eeb391f6f5c
stephanie-wang <swang@cs.berkeley.edu>
2019-03-06 00:58:58 +0000

    Raylet configuration updates

--- benchmarks/cluster-scripts/start_head.sh
+++ benchmarks/cluster-scripts/start_head.sh
@@ -7,4 +7,14 @@ export PATH=/home/ubuntu/anaconda3/bin/:$PATH
 ulimit -c unlimited
 ulimit -n 65536
 ulimit -a
-ray start --head --redis-port=6379 --num-redis-shards $NUM_REDIS_SHARDS --plasma-directory=/mnt/hugepages --huge-pages --internal-config='{"initial_reconstruction_timeout_milliseconds": 200, "num_heartbeats_timeout": 20, "object_manager_repeated_push_delay_ms": 1000}'
+ray start --head \
+  --redis-port=6379 \
+  --num-redis-shards \
+  $NUM_REDIS_SHARDS \
+  --plasma-directory=/mnt/hugepages \
+  --huge-pages \
+  --internal-config='{
+    "initial_reconstruction_timeout_milliseconds": 200,
+    "num_heartbeats_timeout": 20,
+    "object_manager_repeated_push_delay_ms": 1000,
+    "object_manager_pull_timeout_ms": 1000}'
--- benchmarks/cluster-scripts/start_worker.sh
+++ benchmarks/cluster-scripts/start_worker.sh
@@ -4,8 +4,8 @@ HEAD_IP=$1
 NODE_RESOURCE=${2:-'Node'$RANDOM}
 SLEEP_TIME=${3:-$(( $RANDOM % 5 ))}
 
-SEND_THREADS=2
-RECEIVE_THREADS=2
+SEND_THREADS=8
+RECEIVE_THREADS=8
 
 
 export PATH=/home/ubuntu/anaconda3/bin/:$PATH
@@ -18,6 +18,7 @@ echo "Sleeping for $SLEEP_TIME..."
 sleep $SLEEP_TIME
 
 ray start --redis-address=$HEAD_IP:6379 \
+    --num-cpus 2 \
     --resources='{"'$NODE_RESOURCE'": 100}' \
     --plasma-directory=/mnt/hugepages \
     --huge-pages \
@@ -25,5 +26,6 @@ ray start --redis-address=$HEAD_IP:6379 \
     "initial_reconstruction_timeout_milliseconds": 200,
     "num_heartbeats_timeout": 20,
     "object_manager_repeated_push_delay_ms": 1000,
+    "object_manager_pull_timeout_ms": 1000,
     "object_manager_send_threads": '$SEND_THREADS',
     "object_manager_receive_threads": '$RECEIVE_THREADS'}'
