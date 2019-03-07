0a79695c9d0cd1675f3d165c34f418f511eda83e
stephanie-wang <swang@cs.berkeley.edu>
2019-03-07 20:53:44 +0000

    Pin raylet and workers to different cores

--- benchmarks/cluster-scripts/start_worker.sh
+++ benchmarks/cluster-scripts/start_worker.sh
@@ -30,3 +30,13 @@ ray start --redis-address=$HEAD_IP:6379 \
     "object_manager_pull_timeout_ms": 1000,
     "object_manager_send_threads": '$SEND_THREADS',
     "object_manager_receive_threads": '$RECEIVE_THREADS'}'
+
+sleep 5
+taskset -pc 0 `pgrep raylet`
+taskset -pc 0 `pgrep plasma_store`
+renice -n 19 -p `pgrep raylet`
+
+for pid in `pgrep -w python`; do
+    taskset -pc 1-3 $pid
+    #sudo renice -n -5 -p $pid
+done
