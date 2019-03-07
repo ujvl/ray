8308ea85a6c83a3f3091bc16e6ec60b07fab3ba7
stephanie-wang <swang@cs.berkeley.edu>
2019-03-07 22:40:33 +0000

    Make it easier to rebuild ray on cluster

--- benchmarks/cluster-scripts/upgrade_ray.sh
+++ benchmarks/cluster-scripts/upgrade_ray.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 
-cd ~/ray
-git fetch
-git checkout origin/lineage-stash-allreduce
+CHECKOUT=${2:-0}
+
+if [[ $CHECKOUT -ne 0 ]]
+then
+    cd ~/ray
+    git fetch
+    git checkout origin/lineage-stash-allreduce
+fi
 
 (cd ~/ray/build && make -j8) || (find /home/ubuntu/ray/src/ray -name *_generated.h -exec rm {} \; && cd ~/ray/build  && make -j8) || (cd ~/ray/python && python setup.py develop)
+
 
 num_workers=$(( `wc -l ~/workers.txt | awk '{ print $1 }'` - 1 ))
 for worker in `tail -n $num_workers ~/workers.txt`; do
