71fb115aaa1b0d953beec78c9932ca035e17d806
John Liagouris <liagos@inf.ethz.ch>
2019-03-08 07:03:10 -0800

    Run commands with subprocess

--- python/ray/experimental/streaming/benchmarks/micro/run.py
+++ python/ray/experimental/streaming/benchmarks/micro/run.py
@@ -24,51 +24,51 @@ dataflow_parallelism = [1,2]
 fan_in = [2,4,8,16]
 fan_out = [2,4,8,16]
 
-# Batched queue micro-benchmark
-times = "--rounds " + str(rounds) + " "
-period = "--sample-period " + str(100) + " "
-lf = "--latency-file " + latency_filename + " "
-tf = "--throughput-file " + throughput_filename + " "
-command = "python batched_queue_benchmark.py " + times + period + lf + tf
-for num in num_queues:
-    arg1 = "--num-queues " + str(num) + " "
-    for queue_size in max_queue_size:
-        arg2 = "--queue-size " + str(queue_size) + " "
-        for batch_size in max_batch_size:
-            arg3 = "--batch-size " + str(batch_size) + " "
-            for batch_time in batch_timeout:
-                arg4 = "--flush-timeout " + str(batch_time) + " "
-                run = command + arg1 + arg2 + arg3 + arg4
-                print("Executing: ", run)
-                code = subprocess.call(run, shell=True,
-                                    stdout=subprocess.PIPE)
-
-# Chaining micro-benchmark
-times = "--rounds " + str(rounds) + " "
-period = "--sample-period " + str(100) + " "
-lf = "--latency-file " + latency_filename + " "
-tf = "--throughput-file " + throughput_filename + " "
-command = "python api_benchmark.py " + times + period + lf + tf
-for num in num_queues:
-    arg1 = "--num-stages " + str(num) + " "
-    for queue_size in max_queue_size:
-        arg2 = "--queue-size " + str(queue_size) + " "
-        for batch_size in max_batch_size:
-            arg3 = "--batch-size " + str(batch_size) + " "
-            for batch_time in batch_timeout:
-                arg4 = "--flush-timeout " + str(batch_time) + " "
-                for parallelism in dataflow_parallelism:
-                    arg5 = "--dataflow-parallelism " + str(parallelism) + " "
-                    # queue-based
-                    run = command + arg1 + arg2 + arg3 + arg4 + arg5
-                    print("Executing: ", run)
-                    code = subprocess.call(run, shell=True,
-                                        stdout=subprocess.PIPE)
-                    # task-based
-                    run += "--task-based 1"
-                    print("Executing: ", run)
-                    code = subprocess.call(run, shell=True,
-                                        stdout=subprocess.PIPE)
+# # Batched queue micro-benchmark
+# times = "--rounds " + str(rounds) + " "
+# period = "--sample-period " + str(100) + " "
+# lf = "--latency-file " + latency_filename + " "
+# tf = "--throughput-file " + throughput_filename + " "
+# command = "python batched_queue_benchmark.py " + times + period + lf + tf
+# for num in num_queues:
+#     arg1 = "--num-queues " + str(num) + " "
+#     for queue_size in max_queue_size:
+#         arg2 = "--queue-size " + str(queue_size) + " "
+#         for batch_size in max_batch_size:
+#             arg3 = "--batch-size " + str(batch_size) + " "
+#             for batch_time in batch_timeout:
+#                 arg4 = "--flush-timeout " + str(batch_time) + " "
+#                 run = command + arg1 + arg2 + arg3 + arg4
+#                 print("Executing: ", run)
+#                 code = subprocess.call(run, shell=True,
+#                                     stdout=subprocess.PIPE)
+#
+# # Chaining micro-benchmark
+# times = "--rounds " + str(rounds) + " "
+# period = "--sample-period " + str(100) + " "
+# lf = "--latency-file " + latency_filename + " "
+# tf = "--throughput-file " + throughput_filename + " "
+# command = "python api_benchmark.py " + times + period + lf + tf
+# for num in num_queues:
+#     arg1 = "--num-stages " + str(num) + " "
+#     for queue_size in max_queue_size:
+#         arg2 = "--queue-size " + str(queue_size) + " "
+#         for batch_size in max_batch_size:
+#             arg3 = "--batch-size " + str(batch_size) + " "
+#             for batch_time in batch_timeout:
+#                 arg4 = "--flush-timeout " + str(batch_time) + " "
+#                 for parallelism in dataflow_parallelism:
+#                     arg5 = "--dataflow-parallelism " + str(parallelism) + " "
+#                     # queue-based
+#                     run = command + arg1 + arg2 + arg3 + arg4 + arg5
+#                     print("Executing: ", run)
+#                     code = subprocess.call(run, shell=True,
+#                                         stdout=subprocess.PIPE)
+#                     # task-based
+#                     run += "--task-based 1"
+#                     print("Executing: ", run)
+#                     code = subprocess.call(run, shell=True,
+#                                         stdout=subprocess.PIPE)
 
 
 # Fan-in/out micro-benchmark
@@ -93,7 +93,8 @@ for queue_size in max_queue_size:
                 # task-based
                 run += "--task-based 1"
                 print("Executing: ", run)
-                os.system(run)
+                code = subprocess.call(run, shell=True,
+                                    stdout=subprocess.PIPE)
             for fout in fan_out:
                 arg5 = "--fan-out " + str(fout) + " "
                 # queue-based
@@ -104,4 +105,5 @@ for queue_size in max_queue_size:
                 # task-based
                 run += "--task-based 1"
                 print("Executing: ", run)
-                os.system(run)
+                code = subprocess.call(run, shell=True,
+                                    stdout=subprocess.PIPE)
