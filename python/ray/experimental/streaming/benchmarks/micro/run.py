19a8cfa137c79f6983d1c59236c9836ba98cd2b4
John Liagouris <liagos@inf.ethz.ch>
2019-03-28 15:53:58 -0700

    Add fixed source rate parameter to run.py

--- python/ray/experimental/streaming/benchmarks/micro/run.py
+++ python/ray/experimental/streaming/benchmarks/micro/run.py
@@ -11,21 +11,22 @@ import ray
 rounds = 5
 latency_filename = "results/latencies"
 throughput_filename = "results/throughputs"
-_dump_filename = "results/api/dump"
+_dump_filename = "results/dump"
 sample_period = 100
 record_type = "int"
 record_size = None
 max_queue_size = [100]        # in number of batches
 max_batch_size = [1000]       # in number of records
-batch_timeout = [0.01,0.1]
+batch_timeout = [0.1]
 prefetch_depth = 10
 background_flush = False
-num_stages = [2,4]
+num_stages = [1,2,5,10,15,20]
 max_reads_per_second = float("inf")
 partitioning = "round_robin"                # "shuffle", "broadcast"
-dataflow_parallelism = [1,2,4]
+dataflow_parallelism = [1]
 fan_in = [2,4,8,16]
 fan_out = [2,4,8,16]
+source_rate = [40000]
 
 # Task- and queue-based execution micro-benchmark
 times = "--rounds " + str(rounds) + " "
@@ -42,42 +43,19 @@ for num in num_stages:
             arg3 = "--batch-size " + str(batch_size) + " "
             for batch_time in batch_timeout:
                 arg4 = "--flush-timeout " + str(batch_time) + " "
+                for rate in source_rate:
+                    arg5 = "--max-source-rate " + str(rate) + " "
                 # Plain-queue experiment
-                run = cmd_queues + arg1 + arg2 + arg3 + arg4
+                run = cmd_queues + arg1 + arg2 + arg3 + arg4 + arg5
                 code = subprocess.call(run, shell=True,
                                     stdout=subprocess.PIPE)
                 for lop in dataflow_parallelism:
-                    arg5 = "--dataflow-parallelism " + str(lop) + " "
+                    arg6 = "--dataflow-parallelism " + str(lop) + " "
                     # Queue-based execution
-                    run = cmd + arg1 + arg2 + arg3 + arg4 + arg5
+                    run = cmd + arg1 + arg2 + arg3 + arg4 + arg5 + arg6
                     code = subprocess.call(run, shell=True,
                                         stdout=subprocess.PIPE)
                     # Task-based execution
                     run += "--task-based"
                     code = subprocess.call(run, shell=True,
                                         stdout=subprocess.PIPE)
-
-# # Parallelism benchmark
-# times = "--rounds " + str(rounds) + " "
-# period = "--sample-period " + str(100) + " "
-# lf = "--latency-file " + latency_filename + " "
-# tf = "--throughput-file " + throughput_filename + " "
-# command = "python api_benchmark.py " + times + period + lf + tf
-# for num in num_stages:
-#     arg1 = "--num-stages " + str(num) + " "
-#     for queue_size in max_queue_size:
-#         arg2 = "--queue-size " + str(queue_size) + " "
-#         for batch_size in max_batch_size:
-#             arg3 = "--batch-size " + str(batch_size) + " "
-#             for batch_time in batch_timeout:
-#                 arg4 = "--flush-timeout " + str(batch_time) + " "
-#                 for parallelism in dataflow_parallelism:
-#                     arg5 = "--dataflow-parallelism " + str(parallelism) + " "
-#                     # queue-based
-#                     run = command + arg1 + arg2 + arg3 + arg4 + arg5
-#                     code = subprocess.call(run, shell=True,
-#                                         stdout=subprocess.PIPE)
-#                     # task-based
-#                     run += "--task-based"
-#                     code = subprocess.call(run, shell=True,
-#                                         stdout=subprocess.PIPE)
