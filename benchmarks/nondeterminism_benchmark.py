8df4a429a847f16e29774a81a13706b3f236b05d
Stephanie <swang@cs.berkeley.edu>
2019-03-30 13:53:16 -0700

    Add checkpointing to nondeterminism example

--- benchmarks/nondeterminism_benchmark.py
+++ benchmarks/nondeterminism_benchmark.py
@@ -4,6 +4,7 @@ import os
 
 import ray
 from ray.tests.cluster_utils import Cluster
+import ray.cloudpickle as pickle
 
 
 @ray.remote(resources={"Node0": 1})
@@ -32,17 +33,51 @@ class Source(object):
             _, self.queue = ray.wait(self.queue, len(self.queue), timeout=0.1)
 
 @ray.remote(resources={"Node1": 1}, max_reconstructions=100)
-class NondeterministicOperator(object):
+class NondeterministicOperator(ray.actor.Checkpointable):
     def __init__(self, handle):
         self.handle = handle
+        self.iterations = 0
 
     def push(self, record):
         done = self.handle.push.remote(record)
         if record is None:
             ray.get(done)
+        self.iterations += 1
 
     def get_pid(self):
         return os.getpid()
+
+    def should_checkpoint(self, checkpoint_context):
+        if self.iterations > 0 and self.iterations % 100 == 0:
+            return True
+        else:
+            return False
+
+    def save_checkpoint(self, actor_id, checkpoint_id):
+        print("Saving checkpoint", self.iterations, checkpoint_id)
+        checkpoint = {
+                "handle": self.handle,
+                "iterations": self.iterations,
+                "checkpoint_id": checkpoint_id,
+                }
+        checkpoint = pickle.dumps(checkpoint)
+        with open('/home/stephanie/ray-fork/benchmarks/checkpoint', 'wb+') as f:
+            f.write(checkpoint)
+
+    def load_checkpoint(self, actor_id, available_checkpoints):
+        print("Available checkpoints", available_checkpoints)
+        with open('/home/stephanie/ray-fork/benchmarks/checkpoint', 'rb') as f:
+            checkpoint = pickle.loads(f.read())
+        self.handle = checkpoint["handle"]
+        self.iterations = checkpoint["iterations"]
+        self.handle.reset_handle_id()
+        checkpoint_id = checkpoint["checkpoint_id"]
+        print("Reloaded checkpoint", self.iterations, checkpoint_id, flush=True)
+        assert checkpoint_id == available_checkpoints[0].checkpoint_id
+        return checkpoint_id
+
+    def checkpoint_expired(self, actor_id, checkpoint_id):
+        return
 
 @ray.remote(resources={"Node2": 1})
 class Sink(object):
@@ -96,7 +131,7 @@ if __name__ == '__main__':
     num_records = 1000
     generators = [source.generate.remote(num_records) for source in sources]
 
-    time.sleep(1)
+    time.sleep(2)
     node = cluster.list_all_nodes()[-2]
     cluster.remove_node(node)
     node_kwargs["resources"] = {"Node1": 100}
