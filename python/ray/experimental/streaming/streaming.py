fbed047ba1339e396c0fb9d93370338e37e64f77
John Liagouris <liagos@inf.ethz.ch>
2019-03-30 16:59:53 -0700

    Start sources last

--- python/ray/experimental/streaming/streaming.py
+++ python/ray/experimental/streaming/streaming.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import logging
 import math
 import sys
+import time
 import uuid
 
 import networkx as nx
@@ -475,7 +476,7 @@ class Environment(object):
             if not self.config.task_based:
                 for actor_handle in actor_handles:
                     _ = actor_handle.start.remote()  # Start spinning actors
-        time.sleep(1)
+        time.sleep(1)  # Just wait a bit for the actors to start
         # Start sources
         for source_handle in source_handles:
             _ = source_handle.start.remote()
