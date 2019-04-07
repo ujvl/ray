71ee650582ad6f7d822ce6ff8a01145b72968d09
stephanie-wang <swang@cs.berkeley.edu>
2019-04-07 19:02:47 +0000

    init MPI update

--- benchmarks/cluster-scripts/init.sh
+++ benchmarks/cluster-scripts/init.sh
@@ -10,3 +10,16 @@ for host in $(cat ~/workers.txt); do
 done
 
 parallel-ssh -t 0 -i -P -h ~/workers.txt -O "StrictHostKeyChecking=no" -I < enable_hugepages.sh
+
+pushd .
+git clone git@github.com:stephanie-wang/mpi-bench.git ~/mpi-bench
+cd ~/mpi-bench
+bash -x ./build.sh
+popd
+
+num_workers=$(( `wc -l ~/workers.txt | awk '{ print $1 }'` - 1 ))
+for worker in `tail -n $num_workers ~/workers.txt`; do
+    echo $worker
+    rsync -e "ssh -o StrictHostKeyChecking=no" -az "/home/ubuntu/mpi-bench" $worker:/home/ubuntu & sleep 0.5
+done
+wait
