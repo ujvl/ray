52e5e2d00bd0bcc7be8c5b38ef52bb4729ef66ba
stephanie-wang <swang@cs.berkeley.edu>
2019-04-21 21:03:13 +0000

    remove checkpoints when stopping the cluster

--- benchmarks/cluster-scripts/stop_cluster.sh
+++ benchmarks/cluster-scripts/stop_cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 parallel-ssh -t 0 -i -P -h ~/workers.txt -x "-o StrictHostKeyChecking=no -i ~/ray_bootstrap_key.pem" "LC_ALL=C.UTF-8 LANG=C.UTF-8 ray stop && rm -r /tmp/ray/* && pkill yes"
+parallel-ssh -t 0 -i -P -h ~/workers.txt -x "-o StrictHostKeyChecking=no -i ~/ray_bootstrap_key.pem" "rm -r /tmp/ray-checkpoints"
 parallel-ssh -t 0 -i -P -h ~/workers.txt -x "-o StrictHostKeyChecking=no -i ~/ray_bootstrap_key.pem" "pkill -9 python"
