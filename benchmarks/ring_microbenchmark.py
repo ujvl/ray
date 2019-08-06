b6bce302f502dd6b3086e44ae9b22294459a853f
Ujval Misra <ujval@dropbox.com>
2019-08-06 09:30:29 -0700

    Refactoring function and variable names, change throughput metric

--- benchmarks/ring_microbenchmark.py
+++ benchmarks/ring_microbenchmark.py
@@ -22,17 +22,15 @@ from ray.experimental.internal_kv import _internal_kv_get, _internal_kv_put
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-DEBUG = True
+DEBUG = False
+WAIT_MS = 10
+PROGRESS_LOG_FREQUENCe = 500
 CHECKPOINT_DIR = '/tmp/ray-checkpoints'
 
 
 def debug(*args):
     if DEBUG:
-        print(
-            "task ID:",
-            ray.worker.global_worker.current_task_id,
-            *args,
-            flush=True)
+        print(ray.worker.global_worker.current_task_id, *args, flush=True)
 
 
 class RingWorker(object):
@@ -44,6 +42,7 @@ class RingWorker(object):
         self.latencies = []
         self.receiver = None
         self.iteration = 0
+        self.tasks = 0
         self.num_own_tokens_recv = 0
         _internal_kv_put(self.token, self.iteration, overwrite=True)
 
@@ -53,22 +52,25 @@ class RingWorker(object):
     def add_remote_worker(self, worker):
         self.receiver = worker
 
-    @ray.method(num_return_vals=0)
-    def send(self, token, num_tasks_remaining, timestamp):
-        debug("send", token, num_tasks_remaining, timestamp)
+    def num_tasks(self):
+        return self.tasks
 
+    @ray.method(num_return_vals=0)
+    def send(self, token, num_roundtrips_remaining, timestamp):
+        self.tasks += 1
+        #debug("send", token, num_roundtrips_remaining, timestamp)
         if self.task_duration > 0:
             time.sleep(self.task_duration)
 
         if token == self.token:
-            num_tasks_remaining -= 1
+            num_roundtrips_remaining -= 1
             now = time.time()
             self.latencies.append(now - timestamp)
             timestamp = now
 
-        if num_tasks_remaining > 0:
+        if num_roundtrips_remaining > 0:
             # Forward the token to the next actor in the ring.
-            self.receiver.send.remote(token, num_tasks_remaining, timestamp)
+            self.receiver.send.remote(token, num_roundtrips_remaining, timestamp)
         elif token == self.token:
             # We received our token back for the last task in this round.
             self.iteration += 1
@@ -76,65 +78,46 @@ class RingWorker(object):
             debug("DONE", self.token, self.iteration)
 
     def get_latencies(self):
-        # The first task doesn't count because the timestamp was assigned by
-        # the driver.
+        # The first task doesn't count because the timestamp was assigned by the driver.
         latencies = self.latencies[1:][:]
         self.latencies.clear()
         return latencies
 
 
-@ray.remote
-class ProgressTracker(object):
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.pending_tokens = set()
-        self.num_iterations = 0
-
-    def notify(self, token):
-        debug("Received token", token)
-        self.pending_tokens.add(token)
-        if len(self.pending_tokens) == len(self.tokens):
-            self.pending_tokens.clear()
-            self.num_iterations += 1
-            debug("Iteration is now", self.num_iterations)
-
-    def get_num_iterations(self):
-        return self.num_iterations
-
-
-def step(iteration, workers, tokens, num_tasks, task_duration, 
+def step(iteration, workers, tokens, num_roundtrips, task_duration,
         measure_latency=True):
     tasks = []
     for worker, token in zip(workers, tokens):
-        tasks.append(worker.send.remote(token, num_tasks, time.time()))
+        tasks.append(worker.send.remote(token, num_roundtrips, time.time()))
 
     start = time.time()
     next_iteration = iteration + 1
     for token in tokens:
         worker_iteration = int(_internal_kv_get(token))
-        log.debug('For token %s, worker_iter = %s', token, worker_iteration)
+        #log.debug('For token %s, worker_iter = %s', token, worker_iteration)
         while worker_iteration != next_iteration:
-            time.sleep(0.1)
-            log.debug("Waiting for token %s", token)
+            #log.debug("Waiting %s ms for token %s", WAIT_MS, token)
+            time.sleep(WAIT_MS / 1000)
             clients = ray.global_state.client_table()
             failed_clients = [client for client in clients if not client['IsInsertion']]
             assert len(failed_clients) == 0, "Client failed {}".format(failed_clients[0]['NodeManagerAddress'])
             worker_iteration = int(_internal_kv_get(token))
 
-    log.debug("Round %d done after %f", iteration, time.time() - start)
+    if iteration % PROGRESS_LOG_FREQUENCY == 0:
+        log.debug("Step %d done after %f ms", iteration, (time.time() - start) * 1000)
 
     if measure_latency:
         start = time.time()
         latencies = ray.get([worker.get_latencies.remote() for worker in workers])
-        log.debug("Got latencies for round %d after %f", iteration, time.time() - start)
+        log.debug("Got latencies for step %d after %f", iteration, time.time() - start)
 
         latencies = np.array(latencies)
         latencies -= task_duration * (len(workers))
         latencies /= len(workers)
-        latencies = np.reshape(latencies, (num_tasks - 1) * len(workers))
-        log.info("Mean latency round %d: %f", iteration, np.mean(latencies))
-        log.info("Max latency round %d: %f", iteration, np.max(latencies))
-        log.info("Stddev latency round %d: %f", iteration, np.std(latencies))
+        latencies = np.reshape(latencies, (num_roundtrips - 1) * len(workers))
+        log.info("Mean latency step %d: %f", iteration, np.mean(latencies))
+        log.info("Max latency step %d: %f", iteration, np.max(latencies))
+        log.info("Stddev latency step %d: %f", iteration, np.std(latencies))
     else:
         latencies = []
 
@@ -163,7 +146,7 @@ def main(args):
 
     if test_local:
         node_kwargs = {
-            "num_cpus": 2,
+            "num_cpus": 4,
             "object_store_memory": 10**9,
             "_internal_config": internal_config,
         }
@@ -229,36 +212,48 @@ def main(args):
         benchmark_latency(workers, tokens, args)
 
     if args.dump is not None:
+        log.info("Dumping trace...")
         events = ray.global_state.chrome_tracing_dump()
         events += ray.global_state.chrome_tracing_object_transfer_dump()
         with open(args.dump, "w") as outfile:
             json.dump(events, outfile)
 
     if test_local:
+        log.info("Shutting down...")
+        time.sleep(5)
         cluster.shutdown()
 
 
 def benchmark_throughput(workers, tokens, args):
     i = 0
-    log.info("Starting warmup...")
-    warmup_end_ts = time.time() + args.thput_warmup_time
+
+    warmup_duration = args.thput_warmup_duration
+    log.info("Warming up for %s s...", warmup_duration)
+    warmup_end_ts = time.time() + warmup_duration
     while time.time() < warmup_end_ts:
-        log.debug("Starting warmup iteration %d", i)
-        step(i, workers, tokens, args.num_tasks, args.task_duration, False)
+        step(i, workers, tokens, args.num_roundtrips, args.task_duration, False)
         i += 1
     num_warmup_iters = i
         
-    log.info("Starting measurement...")
-    measurement_end_ts = time.time() + args.thput_measurement_time
+    measurement_duration = args.thput_measurement_duration
+    log.info("Measuring for %s s...", measurement_duration)
+    measurement_end_ts = time.time() + measurement_duration 
     while time.time() < measurement_end_ts:
-        log.debug("Starting measurement iteration %d", i)
-        step(i, workers, tokens, args.num_tasks, args.task_duration, False)
+        step(i, workers, tokens, args.num_roundtrips, args.task_duration, False)
         i += 1
 
     num_measurement_iters = i - num_warmup_iters
-    total_tokens = num_measurement_iters * args.num_workers
-    tokens_processed_per_sec = total_tokens / args.thput_measurement_time
-    log.info("Overall throughput: %s tokens/s", tokens_processed_per_sec)
+    # TODO figure out why this calculation is wrong
+    #tasks_per_worker = num_measurement_iters * args.num_roundtrips * args.num_workers
+    #tasks_per_worker_per_sec = tasks_per_worker / args.thput_measurement_duration
+    #tasks_sys_per_sec = tasks_per_worker_per_sec * args.num_workers
+    num_tasks = sum(ray.get([worker.num_tasks.remote() for worker in workers]))
+    sys_throughput = int(num_tasks / measurement_duration)
+    avg_worker_throughput = int(sys_throughput / args.num_workers)
+
+    log.info('Worker throughput (avg): %s tasks/s', avg_worker_throughput)
+    log.info('System throughput: %s tasks/s', sys_throughput)
+    log.info('Steps: %s', num_measurement_iters)
 
 
 def benchmark_latency(workers, tokens, args):
@@ -274,13 +269,13 @@ def benchmark_latency(workers, tokens, args):
     latencies = []
     for i in range(args.num_iterations):
         log.info("Starting iteration %d", i)
-        results = step(i, workers, tokens, args.num_tasks, args.task_duration)
+        results = step(i, workers, tokens, args.num_roundtrips, args.task_duration)
         latencies.append(results)
     
     # Write latency
     if latency_file is not None:
         with open(latency_file, 'a+') as f:
-            log.debug('writing to file')
+            log.info('Writing to ', latency_file)
             for i, latency in enumerate(latencies):
                 for l in latency:
                     f.write('{},{}\n'.format(i, l))
@@ -290,19 +285,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Benchmarks.')
     parser.add_argument(
          '--num-workers',
-         default=4,
+         default=3,
          type=int,
-         help='The number of workers to use.')
+         help='The # of workers to use.')
     parser.add_argument(
-        '--num-tasks',
+        '--num_roundtrips',
         default=100,
         type=int,
-        help='The number of tasks to submit in a round')
-    parser.add_argument(
-        '--num-iterations',
-        default=10,
-        type=int,
-        help='The number of iterations.')
+        help='The # of roundtrips a token takes per step.')
     parser.add_argument(
         '--task-duration',
         default=0,
@@ -319,32 +309,6 @@ if __name__ == "__main__":
         type=str,
         help='A filename to dump the task timeline')
     parser.add_argument(
-        '--record-latency',
-        action='store_true',
-        help='Whether to record the latency')
-    parser.add_argument(
-        '--benchmark-thput',
-        action='store_true',
-        help='Benchmark throughput instead of latency')
-    parser.add_argument(
-        '--thput-warmup-time',
-        default=30,
-        type=int,
-        help='Warmup time (s)')
-    parser.add_argument(
-        '--thput-measurement-time',
-        default=30,
-        type=int,
-        help='Throughput measurement time (s)')
-    parser.add_argument(
-        '--latency-file',
-        default=None,
-        help='File to record the latency')
-    parser.add_argument(
-        '--throughput-file',
-        default=None,
-        help='File to record the throughput')
-    parser.add_argument(
         '--gcs-delay-ms',
         default=-1,
         help='Delay when writing back to GCS. The default is to use the lineage stash.')
@@ -357,6 +321,40 @@ if __name__ == "__main__":
     parser.add_argument(
         '--debug',
         action='store_true')
+    # Latency benchmark options
+    parser.add_argument(
+        '--record-latency',
+        action='store_true',
+        help='Whether to record the latency (latency benchmark only)')
+    parser.add_argument(
+        '--latency-file',
+        default=None,
+        help='File to record the latency (latency benchmark only)')
+    parser.add_argument(
+        '--num-iterations',
+        default=10,
+        type=int,
+        help='The # of iterations (latency benchmark only).')
+    # Throughput benchmark options
+    parser.add_argument(
+        '--benchmark-thput',
+        action='store_true',
+        help='Benchmark throughput instead of latency (throughput benchmark only)')
+    parser.add_argument(
+        '--thput-warmup-duration',
+        default=10,
+        type=int,
+        help='Warmup time in seconds (throughput benchmark only)')
+    parser.add_argument(
+        '--thput-measurement-duration',
+        default=50,
+        type=int,
+        help='Throughput measurement time in seconds (throughput benchmark only)')
+    #parser.add_argument(
+    #    '--throughput-file',
+    #    default=None,
+    #    help='File to record the throughput')
+
     args = parser.parse_args()
 
     main(args)
